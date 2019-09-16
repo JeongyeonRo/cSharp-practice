9983af896ddd5fefad1fd52a4de6219bf1ac0d3f
JeongyeonRo <47593108+JeongyeonRo@users.noreply.github.com>
2019-09-16 21:15:31 +0900

    practice c#
    make calculator

--- WindowsFormsApp0914Hello/WindowsFormsApp0914Hello/calculator.Designer.cs
+++ WindowsFormsApp0914Hello/WindowsFormsApp0914Hello/calculator.Designer.cs
@@ -33,6 +33,21 @@
             this.sum2 = new System.Windows.Forms.TextBox();
             this.sumBtn = new System.Windows.Forms.Button();
             this.sumResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numResult = new System.Windows.Forms.Label();
+            this.subBtn = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.resultBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -41,7 +56,7 @@
             this.label1.BackColor = System.Drawing.SystemColors.Control;
             this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(21, 23);
+            this.label1.Location = new System.Drawing.Point(122, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(105, 12);
             this.label1.TabIndex = 0;
@@ -52,21 +67,21 @@
             // 
             this.sum1.Location = new System.Drawing.Point(23, 58);
             this.sum1.Name = "sum1";
-            this.sum1.Size = new System.Drawing.Size(100, 21);
+            this.sum1.Size = new System.Drawing.Size(82, 21);
             this.sum1.TabIndex = 1;
             // 
             // sum2
             // 
-            this.sum2.Location = new System.Drawing.Point(23, 85);
+            this.sum2.Location = new System.Drawing.Point(111, 58);
             this.sum2.Name = "sum2";
-            this.sum2.Size = new System.Drawing.Size(100, 21);
+            this.sum2.Size = new System.Drawing.Size(76, 21);
             this.sum2.TabIndex = 2;
             // 
             // sumBtn
             // 
-            this.sumBtn.Location = new System.Drawing.Point(143, 58);
+            this.sumBtn.Location = new System.Drawing.Point(193, 58);
             this.sumBtn.Name = "sumBtn";
-            this.sumBtn.Size = new System.Drawing.Size(94, 48);
+            this.sumBtn.Size = new System.Drawing.Size(70, 21);
             this.sumBtn.TabIndex = 3;
             this.sumBtn.Text = "SUM";
             this.sumBtn.UseVisualStyleBackColor = true;
@@ -74,16 +89,183 @@
             // 
             // sumResult
             // 
-            this.sumResult.Location = new System.Drawing.Point(23, 112);
+            this.sumResult.Location = new System.Drawing.Point(269, 58);
             this.sumResult.Name = "sumResult";
-            this.sumResult.Size = new System.Drawing.Size(214, 21);
+            this.sumResult.Size = new System.Drawing.Size(117, 21);
             this.sumResult.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(112, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // numResult
+            // 
+            this.numResult.BackColor = System.Drawing.Color.White;
+            this.numResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.numResult.Font = new System.Drawing.Font("굴림", 12F);
+            this.numResult.Location = new System.Drawing.Point(23, 124);
+            this.numResult.Name = "numResult";
+            this.numResult.Size = new System.Drawing.Size(363, 60);
+            this.numResult.TabIndex = 7;
+            this.numResult.Text = "0";
+            this.numResult.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // subBtn
+            // 
+            this.subBtn.Location = new System.Drawing.Point(311, 254);
+            this.subBtn.Name = "subBtn";
+            this.subBtn.Size = new System.Drawing.Size(75, 25);
+            this.subBtn.TabIndex = 10;
+            this.subBtn.Text = "-";
+            this.subBtn.UseVisualStyleBackColor = true;
+            this.subBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(199, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(81, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(112, 256);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(81, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "8";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(111, 225);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(23, 225);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(82, 23);
+            this.button6.TabIndex = 14;
+            this.button6.Text = "4";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(23, 254);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(82, 23);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(199, 225);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(81, 23);
+            this.button8.TabIndex = 16;
+            this.button8.Text = "6";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(199, 254);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(81, 23);
+            this.button9.TabIndex = 17;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(112, 285);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(81, 23);
+            this.button10.TabIndex = 18;
+            this.button10.Text = "0";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(311, 223);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 25);
+            this.addBtn.TabIndex = 19;
+            this.addBtn.Text = "+";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(311, 196);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 25);
+            this.clearBtn.TabIndex = 20;
+            this.clearBtn.Text = "C";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.ClearBtn_Click);
+            // 
+            // resultBtn
+            // 
+            this.resultBtn.Location = new System.Drawing.Point(311, 285);
+            this.resultBtn.Name = "resultBtn";
+            this.resultBtn.Size = new System.Drawing.Size(75, 25);
+            this.resultBtn.TabIndex = 21;
+            this.resultBtn.Text = "=";
+            this.resultBtn.UseVisualStyleBackColor = true;
+            this.resultBtn.Click += new System.EventHandler(this.AddBtn_Click);
             // 
             // calculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(407, 382);
+            this.ClientSize = new System.Drawing.Size(407, 362);
+            this.Controls.Add(this.resultBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.subBtn);
+            this.Controls.Add(this.numResult);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.sumResult);
             this.Controls.Add(this.sumBtn);
             this.Controls.Add(this.sum2);
@@ -104,6 +286,21 @@
         private System.Windows.Forms.TextBox sum2;
         private System.Windows.Forms.Button sumBtn;
         private System.Windows.Forms.TextBox sumResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label numResult;
+        private System.Windows.Forms.Button subBtn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button resultBtn;
     }
 }
 
--- WindowsFormsApp0914Hello/WindowsFormsApp0914Hello/calculator.cs
+++ WindowsFormsApp0914Hello/WindowsFormsApp0914Hello/calculator.cs
@@ -10,8 +10,20 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp0914Hello
 {
+    //연산자 열거형 데이터 선언
+    public enum operators {add, sub, mul, div };
+
+
     public partial class calculator : Form
     {
+        //더하기 값을 저장하기 위한 변수
+        public int result = 0;
+        //숫자의 시작을 알기 위한 변수 설정
+        public bool isNewNum = true;
+
+        public operators opt = operators.add;
+
+
         public calculator()
         {
             InitializeComponent();
@@ -32,21 +44,181 @@ namespace WindowsFormsApp0914Hello
             //int sum = number1 + number2;
 
             //label1.Text = sum.ToString(); 
-
+            
 
         }
 
         private void SumBtn_Click(object sender, EventArgs e)
         {
+            int number1, number2 = 0;
+
+            //텍스트 박스의 유효성 검사
+            //텍스트 박스에 공백으로 작성하거나 빈칸이 있을 경우 경고 창을 출력
+            //문자를 작성할 경우 경고 창을 출력
+
+            //string객체 안의 함수 - 매개변수로 String 을 받아서 리턴값은 bool
+            //WhiteSpace - 공백문자열, tab
+            //String.IsNullOrWhiteSpace();
+
+            //텍스트박스안이 빈 문자열이면 경고창 발생
+            //if(sum1.Text.Trim() == "" || sum2.Text.Trim() == "")
+            if (String.IsNullOrWhiteSpace(sum1.Text) || 
+                    String.IsNullOrWhiteSpace(sum2.Text))
+            {
+                MessageBox.Show("더할 숫자를 입력해주세요");
+                return;
+            }
+
+            //텍스트 박스의 문자열을 숫자 데이터 타입으로 변경
+            if(int.TryParse(sum1.Text,out number1) == false)
+            {
+                //텍스트 박스의 문자열에 문자가 있을 경우
+                MessageBox.Show("숫자만 입력해주세요");
+                //포커스를 이동
+                sum1.Focus();
+                //작성된 문자열을 선택
+                sum1.SelectAll();
+                return;
+            }
+            if (int.TryParse(sum2.Text, out number2) == false)
+            {
+                //텍스트 박스의 문자열에 문자가 있을 경우
+                MessageBox.Show("숫자만 입력해주세요");
+                //포커스를 이동
+                sum2.Focus();
+                //작성된 문자열을 선택
+                sum2.SelectAll();
+                return;
+            }
+
+            //다른 방법으로 유효성 검사를 하는 방법
+            //-입력창에 제한
+
             //데이터 타입을 변경하기 위해 - convert 툴을 사용
             //텍스트박스의 문자열을 숫자로 변경
-            int number1 = Convert.ToInt32(sum1.Text);
-            int number2 = Convert.ToInt32(sum2.Text);
+            number1 = Convert.ToInt32(sum1.Text);
+            number2 = Convert.ToInt32(sum2.Text);
 
-            int sum = number1 + number2;
+            //int sum = number1 + number2;
+
+            //add 함수를 생성해서 사용하기
+            int sum = add(number1, number2);
 
             //sumResult.Text = Convert.ToString(sum);
             sumResult.Text = sum.ToString();
+        }
+
+        //함수의 프로토 타입 - 데이터 타입을 맞추기
+        public int add(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+        //함수의 오버로딩 - 메소드 중복정의
+        public float add(float num1, float num2)
+        {
+            float sum = num1 + num2;
+            return sum;
+        }
+
+        public int sub(int num1, int num2)
+        {
+            int sub = num1 - num2;
+            return sub;
+        }
+
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            /*
+            if(numResult.Text == "0")
+            {
+                numResult.Text = "1";
+            }else
+            numResult.Text = numResult.Text + "1";
+            */
+
+            Button button1 = (Button)sender;
+            setNum(button1.Text);
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            /*
+            if(numResult.Text == "0")
+            {
+                numResult.Text = "2";
+            }else
+            numResult.Text = numResult.Text + "2";
+            */
+            
+            setNum("2");
+        }
+
+        public void setNum(string num)
+        {
+            if (isNewNum)
+            {
+                //isNewNum의 값이 true일 경우-처음화면 또는 연산버튼이 클릭된 경우
+                // 실행하는 코드 작성
+
+                //새로운 숫자의 시작이 num
+                numResult.Text = num;
+                //새로운 숫자의 시작이 아님을 나타내기 위해 false로 변경
+                isNewNum = false;
+            }
+            else
+            {
+                if (numResult.Text == "0")
+                {
+                    numResult.Text = num;
+                }
+                else
+                {
+                    numResult.Text = numResult.Text + num;
+
+                }
+            }
+        }
+
+        private void SubBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            if (isNewNum == false)
+            {
+                //int num = Convert.ToInt32(numResult.Text);
+                int num = int.Parse(numResult.Text);
+                if (opt == operators.add)
+                    result = result + num;
+                else if (opt == operators.sub)
+                    result = result - num;
+
+                numResult.Text = result.ToString();
+
+                //새로운 숫자를 입력받기 위해 변수 값 변경
+                isNewNum = true;
+            }
+
+            Button optBtn = (Button)sender;
+            if (optBtn.Text == "+")
+                opt = operators.add;
+            else if (optBtn.Text == "-")
+                opt = operators.sub;
+        }
+
+        //계산식을 초기화 시키는 메소드
+        private void ClearBtn_Click(object sender, EventArgs e)
+        {
+            result = 0;
+            isNewNum = true;
+            opt = operators.add;
+            numResult.Text = "0";
         }
     }
 }
