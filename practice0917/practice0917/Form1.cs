eb187c17b4c6675743a86bd2d3b6d0013f8733fc
JeongyeonRo <47593108+JeongyeonRo@users.noreply.github.com>
2019-09-17 17:57:02 +0900

    practice c#
    practice class

---
+++ practice0917/practice0917/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practice0917
+{
+    public partial class Form1 : Form
+    {
+        //
+        private cat mycat = new cat("JoA", 1);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            mycat.play();
+            catScreen.Text = mycat.express();
+
+        }
+
+        private void Feed_Click(object sender, EventArgs e)
+        {
+            mycat.eat();
+            catScreen.Text = mycat.express();
+
+        }
+
+        //프로그램이 시작된 후 5초마다 타이머 작동
+        private void CatTimer_Tick(object sender, EventArgs e)
+        {
+            mycat.borded();
+            catScreen.Text = mycat.express();
+        }
+    }
+
+}
---
+++ practice0917/practice0917/cat.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice0917
+{
+    //public
+    public class cat
+    {
+       public string Name;
+       public int Age;
+
+        //play 함수를 통해서 happiness에 접근
+       private int happiness = 50;
+
+        public cat(string name, int age)
+        {
+           this.Name = name;
+            this.Age = age;
+        }
+
+       public void play()
+        {
+            happiness = happiness + 10;
+
+            if (happiness > 100)
+                happiness = 100;
+
+        }
+
+        public void eat()
+        {
+            happiness = happiness + 20;
+            if (happiness > 100)
+                happiness = 100;
+        }
+
+        public void borded()
+        {
+            happiness = happiness - 10;
+            if (happiness < 0)
+                happiness = 0;
+        }
+        
+        public string express()
+        {
+            string message = "";
+
+            if (happiness > 80)
+                message = "very happy";
+            else if (happiness > 60)
+                message = "happy";
+            else if (happiness >= 40)
+                message = "good";
+            else if (happiness >= 20)
+                message = "bad";
+            else
+                message = "vert bad";
+
+            return this.Name + " : " + message;
+        }
+    }
+}
